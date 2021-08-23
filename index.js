bf44a83575d4df2681729422d6d22a5c2fb9c6ac
duongnguyen17 <duongthptnt@gmail.com>
2021-08-23 11:31:23 +0700

    test 6

--- index.js
+++ index.js
@@ -1,55 +1,55 @@
-// const express = require("express");
-// const app = express();
-// const server = require("http").createServer(app);
-// const io = require("socket.io")(
-//   server
-//   // , {
-//   // 	cors: {
-//   // 		origin: "http://localhost:3000",
-//   // 		methods: [ "GET", "POST" ]
-//   // 	}
-//   // }
-// );
-
-// io.on("connection", (socket) => {
-//   console.log("client connected");
-//   socket.emit("me", socket.id);
-
-//   socket.on("disconnect", () => {
-//     socket.broadcast.emit("callEnded");
-//   });
-
-//   socket.on("callUser", (data) => {
-//     io.to(data.userToCall).emit("callUser", {
-//       signal: data.signalData,
-//       from: data.from,
-//       name: data.name,
-//     });
-//   });
-
-//   socket.on("answerCall", (data) => {
-//     io.to(data.to).emit("callAccepted", data.signal);
-//   });
-// });
-
-// server.listen(3000);
-
-"use strict";
-
 const express = require("express");
-const socketIO = require("socket.io");
-
 const PORT = process.env.PORT || 3000;
-const INDEX = "/index.html";
-
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-const io = socketIO(server);
+const app = express();
+const server = require("http").createServer(app);
+server.listen(PORT);
+const io = require("socket.io")(
+  server
+  // , {
+  // 	cors: {
+  // 		origin: "http://localhost:3000",
+  // 		methods: [ "GET", "POST" ]
+  // 	}
+  // }
+);
 
 io.on("connection", (socket) => {
-  console.log(`${socket.id} connected`);
-  setInterval(() => socket.emit("time", new Date().toTimeString()), 1000);
-  socket.on("disconnect", () => console.log("Client disconnected"));
+  console.log("client connected");
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
+
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("callUser", {
+      signal: data.signalData,
+      from: data.from,
+      name: data.name,
+    });
+  });
+
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
 });
+
+// "use strict";
+
+// const express = require("express");
+// const socketIO = require("socket.io");
+
+// const PORT = process.env.PORT || 3000;
+// const INDEX = "/index.html";
+
+// const server = express()
+//   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+// const io = socketIO(server);
+
+// io.on("connection", (socket) => {
+//   console.log(`${socket.id} connected`);
+//   setInterval(() => socket.emit("time", new Date().toTimeString()), 1000);
+//   socket.on("disconnect", () => console.log("Client disconnected"));
+// });
